1a018dc9c33a25ed85e53911afb43478cdf62f66
Rémi Achard <remi.achard@ymagis.com>
2017-11-20 17:01:37 +0100

    Use reStructuredText instead of Markdown (PyPI description)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'clairmeta/info.py')) as f:
     exec(f.read())
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 
