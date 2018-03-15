bd912d2b0eea4be429c1224c382e2896561c8111
lsde <lsde@users.noreply.github.com>
2018-03-15 16:49:01 +0000

    Update file.py

--- clairmeta/utils/file.py
+++ clairmeta/utils/file.py
@@ -97,27 +97,27 @@ def console_progress_bar(file_path, progress, elapsed, done):
             done (boolean): Completion status.
 
     """
-    col_width = 50
-
-    if not done and LOG_SETTINGS['enable_progressbar']:
-        progress_size = int(progress * col_width)
-        eta_size = col_width - progress_size
-
-        sys.stdout.write("[{}] {:.2f}% - {}\r".format(
-            "{}{}".format('=' * progress_size, ' ' * eta_size),
-            progress * 100.,
-            os.path.basename(file_path)))
-        sys.stdout.flush()
-    else:
-        file_size = os.path.getsize(file_path)
-        speed_report = "{} in {:.2f} sec (at {:.2f} MBytes/s)".format(
-            human_size(file_size), elapsed, (file_size / 1e6) / elapsed)
-
-        sys.stdout.write("[  {}] 100.00% - {}\r".format(
-            speed_report.ljust(col_width - 2),
-            os.path.basename(file_path)))
-        sys.stdout.write("\n")
-
+    if LOG_SETTINGS['enable_progressbar']:
+        col_width = 50
+        if not done:
+            progress_size = int(progress * col_width)
+            eta_size = col_width - progress_size
+    
+            sys.stdout.write("[{}] {:.2f}% - {}\r".format(
+                "{}{}".format('=' * progress_size, ' ' * eta_size),
+                progress * 100.,
+                os.path.basename(file_path)))
+            sys.stdout.flush()
+        else:
+            file_size = os.path.getsize(file_path)
+            speed_report = "{} in {:.2f} sec (at {:.2f} MBytes/s)".format(
+                human_size(file_size), elapsed, (file_size / 1e6) / elapsed)
+    
+            sys.stdout.write("[  {}] 100.00% - {}\r".format(
+                speed_report.ljust(col_width - 2),
+                os.path.basename(file_path)))
+            sys.stdout.write("\n")
+    
 
 def shaone_b64(file_path, callback=None):
     """ Compute file hash using sha1 algorithm.
