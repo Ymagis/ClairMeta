636bdd516959fb3c17ac57aab8e7484219e03446
lsde <lsde@users.noreply.github.com>
2018-03-15 16:50:21 +0000

    Update settings.py

--- clairmeta/settings.py
+++ clairmeta/settings.py
@@ -4,7 +4,7 @@
 LOG_SETTINGS = {
     'level': 'INFO',
     'enable_console': True,
-    'enable_progressbar': True,
+    'enable_progressbar': False,
     'enable_file': True,
     'file_name': '~/Library/Logs/clairmeta.log',
     'file_size': 1e6,
