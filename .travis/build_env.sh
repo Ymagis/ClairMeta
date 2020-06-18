f95d0874ed64cc17437d7139a9376611c69f0a66
Rémi Achard <remiachard@gmail.com>
2020-06-18 22:08:11 +0100

    Fix lxml compile issue (libxslt-dev missing) and end ubuntu trusty support

--- .travis/build_env.sh
+++ .travis/build_env.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-docker build -f .travis/Dockerfile-build-trusty -t clairmeta/build_trusty .
-docker run -it \
-  -v $PWD/.travis:/build_src \
-  -e "BINTRAY_USER=${BINTRAY_USER}" \
-  -e "BINTRAY_ORG=${BINTRAY_ORG}" \
-  -e "BINTRAY_REPO=${BINTRAY_REPO}" \
-  -e "BINTRAY_TOKEN=${BINTRAY_TOKEN}" \
-  -e "DISTRIBUTION=trusty" \
-  clairmeta/build_trusty
+# End support for ubuntu trusty, the ppa we used to have a python 3.6
+# install is no longer working as of June 2020
+# docker build -f .travis/Dockerfile-build-trusty -t clairmeta/build_trusty .
+# docker run -it \
+#   -v $PWD/.travis:/build_src \
+#   -e "BINTRAY_USER=${BINTRAY_USER}" \
+#   -e "BINTRAY_ORG=${BINTRAY_ORG}" \
+#   -e "BINTRAY_REPO=${BINTRAY_REPO}" \
+#   -e "BINTRAY_TOKEN=${BINTRAY_TOKEN}" \
+#   -e "DISTRIBUTION=trusty" \
+#   clairmeta/build_trusty
 
 docker build -f .travis/Dockerfile-build-xenial -t clairmeta/build_xenial .
 docker run -it \
